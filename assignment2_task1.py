ab50bba79635822eef3393c3ff1bb8008ddab0a0
Swapnil Swaraj <swapnilswaraj20@gmail.com>
2025-12-04 20:13:52 +0530

    Add files via upload

---
+++ assignment2_task1.py
@@ -0,0 +1,7 @@
+# This is the assignment 2 Task 1- to print if the input number is even or odd with if and else statement
+
+num=int(input("Enter a number:"))
+if num%2==0:
+    print(num,"is an even number.")
+else:
+    print(num,"is an odd number.")
---
+++ assignment2_task2.py
@@ -0,0 +1,6 @@
+# This is the assignment task to find sum of numbers in range of 1 to 50 by for loop.
+
+sum=int(0)
+for i in range(1,51):
+    sum=sum+i
+print("the sum of numbers from 1 to 50 is:",(sum))
